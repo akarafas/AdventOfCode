700535be2187d5eada5cd8f906b6366e6184bb4f
akarafas <akarafas@live.unc.edu>
2022-03-24 19:50:51 -0400

    Day 1 parts one and two complete

--- src/main/java/day1.java
+++ src/main/java/day1.java
@@ -1,10 +1,13 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class day1 {
     public static void main(String[] args) {
-        part1();
+//        part1();
+//        part2();
     }
     public static void part1() {
         System.out.println("Paste number input and type any char to exit");
@@ -21,7 +24,23 @@ public class day1 {
         scanner.close();
         System.out.println(countInc);
     }
+
     public static void part2() {
-        // todo;
+        ArrayList<Integer> nums = new ArrayList<>();
+        int countInc = 0;
+        Scanner scanner = new Scanner(System.in);
+        nums.add(scanner.nextInt());
+        while (scanner.hasNextInt()) {
+            nums.add(scanner.nextInt());
+        }
+        scanner.close();
+        int i = 0;
+        while (i + 3 < nums.size()) {
+            if (nums.get(i) + nums.get(i+1) + nums.get(i+2) < nums.get(i+1) + nums.get(i+2) + nums.get(i+3)) {
+                countInc++;
+            }
+            i++;
+        }
+        System.out.println(countInc);
     }
 }
