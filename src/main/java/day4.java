fa54e9bd46bb210a1dc3343452951c771ddc2815
akarafas <akarafas@live.unc.edu>
2022-04-10 12:17:36 -0400

    Day 4 part 2

--- src/main/java/day4.java
+++ src/main/java/day4.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 
 public class day4 {
     public static void main(String[] args) {
-        part1();
-//        part2();
+//        part1();
+        part2();
     }
     public static void part1() {
         // collect list of bingo nums to call out and bingo cards
@@ -92,5 +92,69 @@ public class day4 {
             }
         }
         return -2;
+    }
+
+    public static void part2() {
+        // collect list of bingo nums to call out and bingo cards
+        int countInc = 0;
+        Scanner scanner = new Scanner(System.in);
+        String bingoNumsString = scanner.nextLine();
+        String[] bingoNums = bingoNumsString.split(",");
+        ArrayList<String[]> bingoCards = new ArrayList<>();
+        String bingoIntake = "";
+        while (scanner.hasNextLine() && !bingoIntake.equals("exit")) {
+            bingoIntake = scanner.nextLine();
+            if (bingoIntake.equals("exit")) {
+                break;
+            } else if (bingoIntake.strip().isEmpty()) {
+                continue;
+            } else {
+                bingoCards.add(bingoIntake.strip().split(" +"));
+            }
+        }
+        scanner.close();
+
+        int[] winningCounts = new int[bingoCards.size()/5];
+        int puzzleWinnerNum = -1;
+        int bingoNumsWinningNum = 0;
+        int numBingoWins = 0;
+        for (int n = 0; n < bingoNums.length; n++) {
+            for (int i = 0; i < bingoCards.size(); i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (bingoCards.get(i)[j].equals(bingoNums[n])) {
+                        bingoCards.get(i)[j] = "-1";
+                        int winningSpot = checkWin(bingoCards);
+                        if (winningSpot != -2 && winningCounts[winningSpot / 5] == 0) {
+                            winningCounts[winningSpot / 5] = n;
+                            puzzleWinnerNum = i;
+                            bingoNumsWinningNum = Integer.parseInt(bingoNums[n]);
+                            // add win part
+                            System.out.println("bingo " + bingoNumsWinningNum);
+                            numBingoWins++;
+                            // if we are on the last board, calculate the wins
+                            if (numBingoWins <= bingoCards.size()/5) {
+                                puzzleWinnerNum = (puzzleWinnerNum / 5)*5;
+                                int winningSum = 0;
+                                for (int x = 0; x < 5; x++) {
+                                    for (int z = 0; z < 5; z++) {
+                                        if (bingoCards.get(x+puzzleWinnerNum)[z] != "-1") {
+                                            winningSum += Integer.parseInt(bingoCards.get(x+puzzleWinnerNum)[z]);
+                                            bingoCards.get(x+puzzleWinnerNum)[z] = "-3";
+                                        } else {
+                                            bingoCards.get(x+puzzleWinnerNum)[z] = "-3";
+                                        }
+                                    }
+                                }
+                                if (numBingoWins == bingoCards.size()/5) {
+                                    System.out.println(winningSum);
+                                    System.out.println(winningSum * bingoNumsWinningNum);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
